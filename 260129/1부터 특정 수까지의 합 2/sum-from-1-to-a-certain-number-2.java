d9d92ea953f888b9276fa39917645d863e860289
kiddo-psh <kiddo.psh@gmail.com>
2026-01-29 10:21:23 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260129/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int getSum(int n) {
+        if (n == 1) return 1;
+        return getSum(n-1) + n;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(getSum(n));// Please write your code here.
+    }
+}
