b5dfc27307d3e48e572b18fee206408b1be3b809
kiddo-psh <kiddo.psh@gmail.com>
2026-01-31 10:34:15 +0900

    Title: k번째로 신기한 문자열, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260131/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String t = sc.next();
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.next();
+        }
+        Arrays.sort(words);
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<t.length(); j++) {
+                if (words[i].charAt(j)!=t.charAt(j)) {
+                    continue;
+                }
+            }
+            System.out.println(words[i+k]);
+            return;
+        }
+    }
+}
