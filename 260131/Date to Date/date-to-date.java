032060a246d19c8d4cc95e2fad743247e8736468
kiddo-psh <kiddo.psh@gmail.com>
2026-01-31 15:34:42 +0900

    Title: Date to Date, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 260131/Date to Date/date-to-date.java
+++ 260131/Date to Date/date-to-date.java
@@ -9,13 +9,13 @@ public class Main {
         int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int t1 = d1, t2 = d2;
         for (int i=1; i<=12; i++) {
-            if (i<=m1) {
+            if (i<m1) {
                 t1 += num_of_days[i];
             }
-            if (i<=m2) {
+            if (i<m2) {
                 t2 += num_of_days[i];
             }
         } 
-        System.out.println(t2 - t1);
+        System.out.println(t2 - t1 + 1);
     }
 }
