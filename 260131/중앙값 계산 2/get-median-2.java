829411bdf898750f1898a285f1d99166ab6702a4
kiddo-psh <kiddo.psh@gmail.com>
2026-01-31 10:57:04 +0900

    Title: 중앙값 계산 2, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260131/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for (int i=0; i<n; i+=2) {
+            Arrays.sort(arr, 0, i+1);
+            System.out.print(arr[i/2] + " ");
+        }
+    }
+}
