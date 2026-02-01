bed78664014e0de6c046447f47917c1fa5216fbb
kiddo-psh <kiddo.psh@gmail.com>
2026-02-01 16:10:59 +0900

    Title: 흰검 칠하기, Time: 186ms, Memory: 17MB, Status: Passed - Codetree

--- 260201/흰검 칠하기/painting-white-black.java
+++ 260201/흰검 칠하기/painting-white-black.java
@@ -1,36 +1,44 @@
 import java.util.Scanner;
 public class Main {
-    static int[] lines = new int[200_001];
+    static final int MAX_I = 200_001;
+    static int cur = 100_000;
+    static int[] white = new int[MAX_I];
+    static int[] black = new int[MAX_I];
+    static int[] state = new int[MAX_I];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int cur = 100_000;
-        char last = 'c';
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
             for (int j=0; j<x; j++) {
-                if (d == 'R') {
-                    lines[cur+j]++;
-                } else {
-                    lines[cur-j]++;
+                int idx = (d=='R') ? cur+j : cur-j;
+
+                if (state[idx] == 3) continue;
+
+                if (d=='R') {
+                    black[idx]++;
+                    state[idx] = 2;
                 }
+                else {
+                    white[idx]++;
+                    state[idx] = 1;
+                }
+
+                if (white[idx]>=2 && black[idx]>=2) state[idx] = 3;
             }
-            if (d=='R') cur+=x-1;
-            else cur-=x-1;
-            if (i==N-1) last = d;
+            cur += (d=='R') ? (x-1) : (1-x);
         }
-        int black = 0;
-        int white = 0;
-        int grey = 0;
-        for (int i=0; i<200_001; i++) {
-            if (lines[i] == 0) continue;
-            if (lines[i] >= 4) grey++;
-            else if (i < cur) black++;
-            else if (i > cur) white++; 
-            else if (i==cur && last == 'R') black++;
-            else if (i==cur && last == 'L') white++;
+
+        int wCount = 0;
+        int bCount = 0;
+        int gCount = 0;
+        for (int i=0; i<MAX_I; i++) {
+            if (state[i] == 3) gCount++;
+            else if (state[i] == 2) bCount++;
+            else if (state[i] == 1) wCount++;
         }
-        System.out.println(white + " " + black + " " + grey);
+        System.out.println(wCount + " " + bCount + " " + gCount);
     }
 }
