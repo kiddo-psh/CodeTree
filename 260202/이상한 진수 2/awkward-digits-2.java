cb291a3aff3015edd81bd458599615eca443b7ab
kiddo-psh <kiddo.psh@gmail.com>
2026-02-02 15:28:54 +0900

    Title: 이상한 진수 2, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260202/이상한 진수 2/awkward-digits-2.java
+++ 260202/이상한 진수 2/awkward-digits-2.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
+        if (a.equals("1")) {
+            System.out.println(0);
+            return;
+        }
         StringBuilder ans = new StringBuilder().append('1');
         boolean flag = false;
         for (int i=1; i<a.length(); i++) {
