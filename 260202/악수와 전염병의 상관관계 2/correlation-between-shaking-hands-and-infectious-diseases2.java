e71d9c8b1e41601d26da7616581beecebc63f045
kiddo-psh <kiddo.psh@gmail.com>
2026-02-02 10:13:38 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 274ms, Memory: 15MB, Status: Passed - Codetree

--- 260202/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 260202/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -13,25 +13,35 @@ public class Main {
             shakes[i][2] = sc.nextInt();
         }
         Arrays.sort(shakes, (a, b) -> Integer.compare(a[0], b[0]));
-        int[] p = new int[N+1];
-        p[P] = 1;
+        int[][] p = new int[N+1][2];
+        p[P][0] = 1;
+        p[P][1] = 0;
 
         int cnt = 0;
         for (int i=0; i<T; i++) {
-            if (cnt == K) break;
-
             int x = shakes[i][1];
             int y = shakes[i][2];
             
-            if (p[x]==1 || p[y]==1) {
-                p[x] = 1;
-                p[y] = 1;
-                cnt++;
-            } 
+            if (p[x][0]==0 && p[y][0]==0) {
+                continue;
+            } else if (p[x][0]==1 && p[y][0]==1) {
+                p[x][1]++;
+                p[y][1]++;
+            } else if (p[x][0]==1) {
+                if (p[x][1] < K) {
+                    p[x][1]++;   // 감염수 증가
+                    p[y][0] = 1; //감염
+                }           
+            } else if (p[y][0]==1) {
+                if (p[y][1] < K) {
+                    p[y][1]++;
+                    p[x][0] = 1;
+                }
+            }
         }
         StringBuilder output = new StringBuilder();
         for (int i=1; i<=N; i++) {
-            output.append(p[i]);
+            output.append(p[i][0]);
         } 
         System.out.println(output);
     }
