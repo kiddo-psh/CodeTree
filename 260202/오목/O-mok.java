cbd7103feee5c4c2311f4c22eaf12de28375d3f5
kiddo-psh <kiddo.psh@gmail.com>
2026-02-02 21:24:19 +0900

    Title: 오목, Time: 151ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 260202/오목/O-mok.java
+++ 260202/오목/O-mok.java
@@ -3,45 +3,30 @@ import java.util.Scanner;
 public class Main {
     static int[] result = new int[2];
     static int[][] arr;
+    static int[] dr = {-1, 0, -1};
+    static int[] dc = {0, 1, 1};
     static boolean isWin(int r, int c, int n) {
         boolean flag = true;
-        for (int i=1; i<=4; i++) {
-            if(arr[r+i][c]!=n) {
-                flag = false;
-                break;
+        for (int i=0; i<3; i++) {
+            flag = true;
+            for (int j=1; j<=4; j++) {
+                int nr = r + j*dr[i];
+                int nc = c + j*dc[i];
+                if (!inRange(nr, nc) || arr[nr][nc] != n) {
+                    flag = false;
+                    break;
+                }
             }
-            if(i==4) {
-                result[1] = c+1;
-                result[0] = r+3;
-            }
-        }
-        if (flag) return true;
-
-        flag = true; 
-        for (int i=1; i<=4; i++) {
-            if(arr[r][c+i]!=n) {
-                flag = false;
-                break;
-            }
-            if(i==4) {
-                result[1] = c+3;
-                result[0] = r+1;
-            }
-        }
-        if (flag) return true;
-
-        flag = true; 
-        for (int i=1; i<=4; i++) {
-            if(arr[r+i][c+i]!=n) {
-                flag = false;
-                break;
-            }
-            if(i==4) {
-                result[1] = c+3;
-                result[0] = r+3;
+            if (flag) {
+                result[0] = r + 1 + 2*dr[i];
+                result[1] = c + 1 + 2*dc[i];
+                return flag;
             }
         }
         return flag;
+    }
+    static boolean inRange(int r, int c) {
+        return r>=0 && r<19 && c>=0 && c<19;
     }
 
     public static void main(String[] args) {
@@ -63,6 +48,7 @@ public class Main {
                 }
             }
         }
+        System.out.println(0);
         // Please write your code here.
     }
 }
