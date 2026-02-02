5e4c3fcdc0b83e3df426d7def6381916d933a7e9
kiddo-psh <kiddo.psh@gmail.com>
2026-02-02 21:56:04 +0900

    Title: 최고의 13위치 2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 260202/최고의 13위치 2/best-place-of-13-2.java
+++ 260202/최고의 13위치 2/best-place-of-13-2.java
@@ -13,12 +13,12 @@ public class Main {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n-2; j++) { 
                 int s = arr[i][j] + arr[i][j+1] + arr[i][j+2];
-                if (i+3 <= n-3) {
-                    for (int k=i+3; k<n-2; k++) {
+                //if (i+3 <= n-3) {
+                    for (int k=j+3; k<n-2; k++) {
                         sum = s + arr[i][k] + arr[i][k+1] + arr[i][k+2];
                         ans = Math.max(ans, sum);
                     }
-                }
+                //}
                 for (int k=i+1; k<n; k++) {
                     for (int l=0; l<n-2; l++) {
                         sum = s + arr[k][l] + arr[k][l+1] + arr[k][l+2];
