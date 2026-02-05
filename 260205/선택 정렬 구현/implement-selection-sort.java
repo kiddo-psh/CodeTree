91b5692302c975be17fd316c4b79fbdb279938d8
kiddo-psh <kiddo.psh@gmail.com>
2026-02-05 15:26:45 +0900

    Title: 선택 정렬 구현, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260205/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0; i<n; i++) {
+            int minIdx = i;
+            for (int j=i; j<n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+        for (int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
