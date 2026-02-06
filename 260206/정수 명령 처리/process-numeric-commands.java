beda061342934a5e43e6f778d9b0f968077ecf61
kiddo-psh <kiddo.psh@gmail.com>
2026-02-06 15:35:17 +0900

    Title: 정수 명령 처리, Time: 346ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260206/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i<n; i++) {
+            String command = sc.next();
+            if (command.equals("push")) {
+                int val = sc.nextInt();
+                stack.push(val);
+            } else if (command.equals("size")) {
+                System.out.println(stack.size());
+            } else if (command.equals("empty")) {
+                if (stack.isEmpty()) System.out.println(1);
+                else System.out.println(0);
+            } else if (command.equals("pop")) {
+                System.out.println(stack.pop());
+            } else if (command.equals("top")) {
+                System.out.println(stack.peek());
+            }
+        }
+    }
+}
