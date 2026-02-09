17eb9f627ff88f6070355d9a81c2d34ffa171660
kiddo-psh <kiddo.psh@gmail.com>
2026-02-09 22:38:31 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260209/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+  
+public class Main {
+    static int result = 0;
+    static int[][] grid;
+    static int n, m;
+
+    static void findRow() {
+        for (int i=0; i<n; i++) {
+            int num = grid[i][0];
+            int count = 1;
+            for (int j=1; j<n; j++) {
+                if (count == m) {
+                    result++;
+                    break;
+                }
+                if (num != grid[i][j]) {
+                    num = grid[i][j];
+                    count = 1;
+                } else {
+                    count++;
+                }
+                if (count == m) {
+                    result++;
+                    break;
+                }
+            }
+        }
+    }
+    static void findCol() {
+        for (int i=0; i<n; i++) {
+            int num = grid[0][i];
+            int count = 1;
+            for (int j=1; j<n; j++) {
+                if (count == m) {
+                    result++;
+                    break;
+                }
+                if (num != grid[j][i]) {
+                    num = grid[j][i];
+                    count = 1;
+                } else {
+                    count++;
+                }
+                if (count == m) {
+                    result++;
+                    break;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        n = sc.nextInt();
+        m = sc.nextInt();
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        findRow();
+        findCol();
+
+        System.out.println(result);
+    }
+}
