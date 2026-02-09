09e1d3f27b936ecece627f19b4615cdba1539a46
kiddo-psh <kiddo.psh@gmail.com>
2026-02-09 22:22:28 +0900

    Title: 최고의 33위치, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260209/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    static int[][] grid;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        int result = calc(0,0);
+        for (int i=0; i<n-3; i++) {
+            for (int j=0; j<n-3; j++) {
+                int sum = calc(i, j);
+                result = Math.max(sum, result);
+            }
+        }
+
+        System.out.println(result);
+        // Please write your code here.
+    }
+    static int calc (int r, int c) {
+        int ret = 0;
+        for (int i=r; i<r+3; i++) {
+            for (int j=c; j<c+3; j++) {
+                ret += grid[i][j];
+            }
+        }
+        return ret;
+    }
+}
