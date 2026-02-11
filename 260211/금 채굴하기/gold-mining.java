cbe2d07554cd5d2600279f4baac65143b79cba3d
kiddo-psh <kiddo.psh@gmail.com>
2026-02-11 14:42:28 +0900

    Title: 금 채굴하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260211/금 채굴하기/gold-mining.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][] grid;
+    static int n, m;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        int result = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                for (int k=0; k<n; k++) {
+                    int gold = digging(i, j, k); // n 크기로 채굴
+                    result = Math.max(gold, result);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    static int digging(int r, int c, int k) {
+        if (r+k>=n || r-k<0 || c-k<0 || c+k>=n) return -1;
+        int count = 0;
+        int cost = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (Math.abs(i-r)+Math.abs(j-c) <= k) {
+                    cost++;
+                    if (grid[i][j] == 1) count++;
+                }
+            }
+        }
+        
+        if (count*m - cost < 0) return -1;
+        return count;
+    }
+}
