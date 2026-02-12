ebdb5e8dd9c4ab07c6fd6b5aaeafbb36624b3783
kiddo-psh <kiddo.psh@gmail.com>
2026-02-13 00:16:15 +0900

    Title: 떨어지는 1자 블록, Time: 405ms, Memory: 23MB, Status: Wrong Answer - Codetree

---
+++ 260213/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt()-1;
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        boolean flag;
+        for (int row=n-1; row>=0; row--) {
+            flag = true;
+            for (int col=k; col<k+m; col++) {
+                if (grid[row][col]!=0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                for (int col=k; col<k+m; col++) {
+                    grid[row][col] = 1;
+                }   
+                break;
+            }
+        }
+
+        for (int[] arr : grid) {
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
