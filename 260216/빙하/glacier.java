d9ed4f93e5231555d3b5c67adb0dc31a97cf352d
kiddo-psh <kiddo.psh@gmail.com>
2026-02-16 23:14:53 +0900

    Title: 빙하, Time: 917ms, Memory: 50MB, Status: Passed - Codetree

--- 260216/빙하/glacier.java
+++ 260216/빙하/glacier.java
@@ -2,11 +2,13 @@ import java.util.*;
 public class Main {
     static int n, m, cnt, ans;
     static int[][] grid;
+    static boolean[][] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
         grid = new int[n][m];
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 grid[i][j] = sc.nextInt();
@@ -21,43 +23,60 @@ public class Main {
     }
 
     static boolean bfs() {
-        int[][] temp = new int[n][m];
-        for (int i=0; i<n; i++) temp[i] = grid[i].clone();
+        visitedClear();
+        q.clear();
+        q.offer(new int[]{0,0});
+        visited[0][0] = true;
 
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int i=0; i<4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if (!inRange(nr, nc)) continue;
+                if (grid[nr][nc]==1) continue;
+                if (visited[nr][nc]) continue;
+
+                visited[nr][nc] = true;
+                q.offer(new int[]{nr, nc});
+
+            }
+        }
         cnt = 0;
+        for (int i=1; i<n-1; i++) {
+            for (int j=1; j<m-1; j++) {
+                if (grid[i][j]==0) continue;
 
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (grid[i][j]==1) continue;
-                    if (i!=0&&j!=0&&i!=n-1&&j!=n-1&&surrounded(i, j)) continue; // 사방이 빙하
+                for (int d=0; d<4; d++) {
+                    int nr = i + dr[d];
+                    int nc = j + dc[d];
 
-                    for (int d=0; d<4; d++) {
-                        int nr = i + dr[d];
-                        int nc = j + dc[d];
-
-                        if (!inRange(nr,nc)) continue;
-
-                        temp[nr][nc] = 0;
+                    if (!inRange(nr, nc)) continue;
+                    if (grid[nr][nc]==1) continue;
+                    if (visited[nr][nc]) {
+                        cnt++;
+                        grid[i][j] = 0;
+                        break;
                     }
-                
+                }
             }
         }
-
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (grid[i][j]==1) cnt1++;
-                if (temp[i][j]==1) cnt2++;
-            }
-        }
-
-        cnt = cnt1 - cnt2;
-        grid = temp;
-        //System.out.println(cnt);
         
-        if (cnt2==0) return false;
-        return true;
+        for (int[] arr : grid) {
+            for (int x : arr) {
+                if (x == 1) return true;
+            }
+        }
+        return false;
+    }
+
+    static void visitedClear() {
+        for (int i=0; i<n; i++) for (int j=0; j<m; j++)
+            visited[i][j] = false;
     }
 
     static final int[] dr = {-1,1,0,0};
@@ -67,33 +86,5 @@ public class Main {
 
     static boolean inRange (int r, int c) {
         return r>=0 && r<n && c>=0 && c<m;
-    }
-
-    static boolean surrounded (int r, int c) {
-        q.clear();
-        q.offer(new int[]{r, c});
-        
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int cr = cur[0];
-            int cc = cur[1];
-
-            for (int i=0; i<4; i++) {
-                int nr = cr + dr[i];
-                int nc = cc + dc[i];
-
-                if (!inRange(nr, nc)) continue;
-                if (grid[nr][nc]==1) continue;
-
-                if (nr==1 || nc==1 || nr==n-1 || nc==n-1) {
-                    return false;
-                }
-
-                q.offer(new int[] {nr, nc});
-
-            }
-        }
-
-        return true;
     }
 }
