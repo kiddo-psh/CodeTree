1c3ea16e4ce41251886af762eb08196c538d5e37
kiddo-psh <kiddo.psh@gmail.com>
2026-02-20 22:12:52 +0900

    Title: 아름다운 수, Time: 113ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260220/아름다운 수/beautiful-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    static int n, answer;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        answer = 0;
+        dfs(0);
+        System.out.println(answer);
+    }
+
+    static void dfs(int depth) {
+        if (depth>n) {return;}
+        if (depth==n) {
+            answer++;
+            return;
+        }
+
+        for (int i=1; i<=9; i++) {
+            dfs(depth+i);
+        }
+    }
+}
