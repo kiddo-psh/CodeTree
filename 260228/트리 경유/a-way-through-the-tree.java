78c2cabe58712e9f3a79339c31ec12de7706dc11
kiddo-psh <kiddo.psh@gmail.com>
2026-02-28 00:26:23 +0900

    Title: 트리 경유, Time: 299ms, Memory: 26MB, Status: Wrong Answer - Codetree

---
+++ 260228/트리 경유/a-way-through-the-tree.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder output = new StringBuilder();
+    static int N, Q;
+    static int[] bt;
+
+    static void query(int node) {
+        boolean isPossible = true;
+        int next = node;
+        int cause = -1;
+        while (next != 0) {
+            next /= 2;
+            if (bt[next] == 1) {
+                isPossible = false;
+                cause = next;
+            }
+        }
+
+        if (isPossible) {
+            bt[node] = 1;
+            output.append(0).append("\n");
+        } else {
+            output.append(cause).append("\n");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+        bt = new int[N+1];
+
+        for (int i=0; i<Q; i++) {
+            query(Integer.parseInt(br.readLine()));
+        }
+
+        System.out.println(output);
+        br.close();
+    }
+}
